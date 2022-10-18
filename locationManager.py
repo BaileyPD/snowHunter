7e1cbb58ccbced3ebc477656ab7567ef728d3e15
Michael Bailey <Baileypdservices@gmail.com>
2022-10-18 16:33:00 -0400

    Created locations data write functionality

--- locationManager.py
+++ locationManager.py
@@ -1,22 +1,42 @@
+# TODO Add .csv read/write functionality
+# TODO Add conversion from location name to lat/long
+# TODO Add return of list of stored locations
+import pandas as pd
+from os.path import exists
 
-#TODO Add .csv read/write functionality
-#TODO Add conversion from location name to lat/long
-#TODO Add return of list of stored locations
+location_file = '/Users/mike/PycharmProjects/snowHunter/locations.csv'
 
-
-
-def newLocation (lat, long):
-
-
-#Function to return a list of locations with names, latitudes, longitudes
+location_header = ['Detailed Name', 'Actual Name', 'latitude', 'longitude']
 
 
-def returnLoocations():
+# def newLocation (lat, long):
+#
+
+# Function to return a list of locations with names, latitudes, longitudes
 
 
-def readLocation():
+# def returnLoocations():
+#
+#
+# def readLocation():
+#
+#TODO Change to pandas.concat instead of append
+def write_location(detail_name: str, actual_name: str, long: float, lat: float) -> pd.DataFrame:
+    # Checks if locations.csv exists, if not sets it up with proper headers
+    if exists(location_file):
+        data = pd.read_csv(filepath_or_buffer=location_file, header= 0)
+        print("Found file")
+        print(data)
+    else:
+        data = pd.DataFrame(columns=location_header)
 
-
-def writeLocation():
-
-
+    # takes function inputs and puts them in the appropriate format
+    data_entry = [[detail_name, actual_name, long, lat]]
+    # Puts new data in proper data frame format
+    added_data = pd.DataFrame(columns=location_header, data=data_entry)
+    # Appends data to existing data
+    data = data.append(other=added_data, ignore_index=False)
+    print(data)
+    # Returns data to CSV file
+    data.to_csv(path_or_buf=location_file,index=False)
+    return data
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import weatherService as ws
+import locationManager as lm
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ws.getWeather()
+    lm.write_location(detail_name="Test3", actual_name="Test 5", long= 56789, lat=912345)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
