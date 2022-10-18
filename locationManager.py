b49597f913a81adff3befcbdb11fe64c52eaf22d
Michael Bailey <Baileypdservices@gmail.com>
2022-10-18 15:50:46 -0400

    Outlined basic location management functionality

---
+++ locationManager.py
@@ -0,0 +1,22 @@
+
+#TODO Add .csv read/write functionality
+#TODO Add conversion from location name to lat/long
+#TODO Add return of list of stored locations
+
+
+
+def newLocation (lat, long):
+
+
+#Function to return a list of locations with names, latitudes, longitudes
+
+
+def returnLoocations():
+
+
+def readLocation():
+
+
+def writeLocation():
+
+
