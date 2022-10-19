c6a7ad0899b0ceb5792c906cc8d87671c7a0760e
Michael Bailey <Baileypdservices@gmail.com>
2022-10-19 13:20:44 -0400

    WeatherService.py now retrieves weather

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import weatherService as ws
 import locationManager as lm
 
 if __name__ == '__main__':
-    menuManager.edit_locations()
+    #menuManager.edit_locations()
     #menuManager.menu_main()
     # lm.write_location(detail_name="Test3", actual_name="Test 5", long= 56789, lat=912345)
     # print("Printing Locations: ")
     # print(lm.return_locations())
-    # ws.get_weather(34.850750, -82.398956)
+    ws.get_weather(34.850750, -82.398956)
+    ws.get_weather(46.371739, -84.714879)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- weatherService.py
+++ weatherService.py
@@ -8,12 +8,11 @@ config.read(configFilePath)
 
 api_key = config['general']['apikey']
 
-Base_URL = 'http://api.openweathermap.org/data/2.5/forecast'
+Base_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 
 def get_weather(latitude: float, longitude: float):
-    print(api_key)
-    URL = '?lat={' + str(latitude) + "}&lon={" + str(longitude) + "}&appid={" + api_key + "}"
+    URL = '?lat=' + str(latitude) + "&lon=" + str(longitude) + "&appid=" + api_key + "&units=imperial"
     print(URL)
     combined_url = Base_URL + URL
     print(combined_url)
@@ -21,7 +20,11 @@ def get_weather(latitude: float, longitude: float):
     print(response)
     if response.status_code == 200:
         data = response.json()
-        main = data['Main']
-        print(main)
+        print(data)
+        data_list = data['list']
+        for x in data_list:
+            print("Temperature: " + str(x['main']['temp']) + "F at " + str(x['main']['humidity']) + " percent humidity" )
+            print(x['weather'])
+        #print(main)
 
 # api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
